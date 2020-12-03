3b7d72404ba57baefc6417f23ff69a324f7d35c4
Matt Eden <mrmatteden@gmail.com>
2020-12-03 21:18:24 +1300

    Complete Part 2 of Day 3

--- 2020/day-3/ans.cpp
+++ 2020/day-3/ans.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <ctime>
+#include <stdint.h>
 
 using namespace std;
 
@@ -24,14 +25,16 @@ int loadDataFromFile(string path, vector<string> &vals) {
   return 0;
 }
 
-int findSolutionPart1(const vector<string> &vals) {
+int findSolutionPart1(const vector<string> &vals, bool verbose) {
   int width = vals.at(0).length();
   int pos = 3;
   int numTrees = 0;
   string line;
 
-  cout << "Width: " << width << endl;
-  cout << vals.at(0) << endl;
+  if (verbose) {
+    cout << "Width: " << width << endl;
+    cout << vals.at(0) << endl;
+  }
 
   // Skip the first row
   for (unsigned int i = 1; i < vals.size(); i++) {
@@ -39,13 +42,13 @@ int findSolutionPart1(const vector<string> &vals) {
     if (pos >= width) pos -= width;
 
     if (line[pos] == '#') {
-      line.replace(pos, 1, "X");
+      if (verbose) line.replace(pos, 1, "X");
       numTrees++;
-    } else {
+    } else if (verbose) {
       line.replace(pos, 1, "O");
     }
 
-    cout << line << " ||" << pos << endl;
+    if (verbose) cout << line << " ||" << pos << endl;
 
     pos += 3;
   }
@@ -54,7 +57,46 @@ int findSolutionPart1(const vector<string> &vals) {
 
 }
 
-void printSolutionsAndTiming(int result, clock_t start, clock_t stop) {
+int64_t findSolutionPart2(const vector<string> &vals, bool verbose, int right, int down) {
+  int width = vals.at(0).length();
+  int pos = right;
+  int64_t numTrees = 0;
+  string line;
+
+  if (verbose) {
+    cout << "Width: " << width << endl;
+    cout << vals.at(0) << endl;
+  }
+
+  // Skip the first row
+  for (unsigned int i = 1; i < vals.size(); i++) {
+
+    line = vals.at(i);
+
+    if (i % down != 0) {
+      if (verbose) cout << line << " ||" << pos << endl;
+      continue;
+    }
+
+    if (pos >= width) pos -= width;
+
+    if (line[pos] == '#') {
+      if (verbose) line.replace(pos, 1, "X");
+      numTrees++;
+    } else if (verbose) {
+      line.replace(pos, 1, "O");
+    }
+
+    if (verbose) cout << line << " ||" << pos << endl;
+
+    pos += right;
+  }
+
+  return numTrees;
+
+}
+
+void printSolutionsAndTiming(int64_t result, clock_t start, clock_t stop) {
   if (result == -1) cout << "No solution found" << endl;
   else {
     cout << "Solution: " << result << endl;
@@ -68,24 +110,29 @@ int main(int argc, char ** argv) {
   cout << "## Day 3 of Advent of Code ##" << endl;
 
   vector<string> vals;
-  int result = -1;
+  int64_t result = 0;
   clock_t start, stop;
 
   loadDataFromFile("input", vals);
 
   cout << "-- Part 1 --" << endl;
   start = clock();
-  result = findSolutionPart1(vals);
+  result = findSolutionPart1(vals, false);
   stop = clock();
 
   printSolutionsAndTiming(result, start, stop);
-  /*
+  
   cout << "-- Part 2 --" << endl;
+  
   start = clock();
-  result = findSolutionPart2(vals);
+  result = findSolutionPart2(vals, false, 1, 1);
+  result *= findSolutionPart2(vals, false, 3, 1);
+  result *= findSolutionPart2(vals, false, 5, 1);
+  result *= findSolutionPart2(vals, false, 7, 1);
+  result *= findSolutionPart2(vals, false, 1, 2);
   stop = clock();
 
   printSolutionsAndTiming(result, start, stop);
-  */
+
 }
 
