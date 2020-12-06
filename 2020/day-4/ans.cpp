320a8af71a79df2674f22c52959a2f7c5eb97a22
Matt Eden <mrmatteden@gmail.com>
2020-12-06 23:18:14 +1300

    Complete Part 2 of Day 4

--- 2020/day-4/ans.cpp
+++ 2020/day-4/ans.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <set>
 #include <algorithm>
+#include <regex>
 #include <ctime>
 
 using namespace std;
@@ -19,11 +20,110 @@ vector<string> split(const string &str, const string &delim = " ") {
   return tokens;
 }
 
+// Some magic C bs
+bool validateStringAsInt(const string &val) {
+  char *end;
+  long i = strtol(val.c_str(), &end, 10);
+  return *end == '\0';
+}
+
+bool validateYear(const string &val, int min, int max) {
+
+  // Validate numberness of string itself
+  if (val.size() != 4) return false;
+  if (!validateStringAsInt(val)) return false;
+
+  // Validate date range
+  int year = stoi(val);
+  return year >= min && year <= max;
+
+}
+
+// Not good code
+bool validateHeight(const string &val) {
+  
+  size_t sz;
+  int num = stoi(val, &sz);
+  string suffix = val.substr(sz);
+
+  if (suffix == "in") return num >= 59 && num <= 76;
+  else if (suffix == "cm") return num >= 150 && num <= 193;
+
+  return false;
+
+}
+
+// Using if ladder instead of trying to use maps/hashes for string switch
+// (also, didn't feel like using regex)
+bool validateField(const string& field, const string& val) {
+
+  set<string> eyeColours = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+  regex hairColourPattern("^#(?=[0-9a-f]{6}$).*");
+
+  // Four digits between 1920 & 2002 (inclusive)
+  if (field == "byr") return validateYear(val, 1920, 2002);
+  // Four digits between 2010 & 2020 (inclusive)
+  if (field == "iyr") return validateYear(val, 2010, 2020);
+  // Four digits between 2020 & 2030 (inclusive)
+  if (field == "eyr") return validateYear(val, 2020, 2030);
+  /*
+   * For cm: 150 -> 193 (inclusive) 
+   * For in: 59 -> 76 (inclusive)
+   * (guess they don't like short people and/or children)
+   * */
+  if (field == "hgt") return validateHeight(val);
+  // Must start with # and followed by exactly 6 chars 0-9 / a-f
+  // (okay I caved into regex)
+  if (field == "hcl") return regex_match(val, hairColourPattern);
+  // Compared against above set
+  if (field == "ecl") return eyeColours.find(val) != eyeColours.end();
+  // Nine digit number including leading zeroes
+  if (field == "pid") return validateStringAsInt(val) && val.size() == 9;
+  return false;
+}
+
 int findSolutionPart1(const string &path, const set<string> &fields, const set<string> &ignored) {
   string line;
   ifstream inFile(path);
   size_t pos = 0;
   string idField;
+  short int numFields = 0;
+  short int numRequired = fields.size() - ignored.size();
+  int valid = 0;
+
+  /* Process data as it's loaded from file */
+  if (inFile.is_open()) {
+    while (getline(inFile, line)) {
+      // Determine validity of passport on blank lines
+      if (line == "") {
+        if (numFields == numRequired) valid++;
+        numFields = 0;
+        continue;
+      }
+
+      // Check each id in line against lists
+      for (string s : split(line)) {
+        idField = split(s,":").at(0);
+        // Ignored ones don't affect validity
+        if (ignored.find(idField) != ignored.end()) continue;
+        // Count valid fields
+        if (fields.find(idField) != fields.end()) numFields++;
+      }
+    }
+    inFile.close();
+  }
+  else { cout << "Error in opening file" << endl; return -1; }
+
+  if (numFields == numRequired) valid++;
+
+  return valid;
+}
+
+int findSolutionPart2(const string &path, const set<string> &fields, const set<string> &ignored) {
+  string line;
+  ifstream inFile(path);
+  size_t pos = 0;
+  vector<string> pair;
   short int numFields = 0;
   short int numRequired = fields.size() - ignored.size();
   int valid = 0;
@@ -43,15 +143,15 @@ int findSolutionPart1(const string &path, const set<string> &fields, const set<s
 
       // Check each id in line against lists
       for (string s : split(line)) {
-        idField = split(s,":").at(0);
-        cout << "Checking ID: " << idField << endl;
+        pair = split(s,":");
+        cout << "Checking ID: " << pair.at(0) << endl;
         // Ignored ones don't affect validity
-        if (ignored.find(idField) != ignored.end()) {
+        if (ignored.find(pair.at(0)) != ignored.end()) {
           cout << "-!> Optional field" << endl;
           continue;
         }
         // Count valid fields
-        if (fields.find(idField) != fields.end()) {
+        if (fields.find(pair.at(0)) != fields.end() && validateField(pair.at(0), pair.at(1))) {
           numFields++;
           cout << "-> Found in list!" << endl;
         }
@@ -111,13 +211,13 @@ int main(int argc, char ** argv) {
 
   printSolutionsAndTiming(result, start, stop);
 
-  /*cout << "-- Part 2 --" << endl;
+  cout << "-- Part 2 --" << endl;
   
   start = clock();
-  result = findSolutionPart2(vals, false, 1, 1);
+  result = findSolutionPart2("input", fields, ignored);
   stop = clock();
 
-  printSolutionsAndTiming(result, start, stop);*/
+  printSolutionsAndTiming(result, start, stop);
 
 }
 
