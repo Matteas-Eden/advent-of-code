d538d67c133f77762662b335094802e9d6bd428a
Matt Eden <mrmatteden@gmail.com>
2020-12-16 15:03:17 +1300

    Complete Part 2 of Day 10

--- 2020/day-10/ans.cpp
+++ 2020/day-10/ans.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <ctime>
+#include <stdint.h>
 
 using namespace std;
 
@@ -24,7 +25,7 @@ int loadDataFromFile(string path, vector<int> &vals) {
   return 0;
 }
 
-int findSolutionPart1(const vector<int> &vals, bool verbose = false) {
+int64_t findSolutionPart1(const vector<int> &vals, bool verbose = false) {
 
   vector<int> sorted(vals);
   vector<int>::const_iterator it = sorted.begin() + 1;
@@ -47,6 +48,76 @@ int findSolutionPart1(const vector<int> &vals, bool verbose = false) {
 
 }
 
+/*
+ * I don't full understand how this function came to be, but it's derived from
+ * a similar function here: https://bit.ly/3gYJfT6.
+ *
+ * In that person's code, they give justifications to how they arrived at the
+ * hardcoded values in the switch statement based on an investigation of the
+ * diffs in their input.
+ * */
+int calculateCombinations(int sublistLength) {
+
+  if (sublistLength <= 1) return 1;
+
+  int valid = 1;
+
+  switch (sublistLength) {
+    case 2:
+      break;
+    case 3:
+      valid += 1;
+      break;
+    case 4:
+      valid += 3;
+      break;
+    case 5:
+      valid += 6;
+      break;
+  }
+
+  return valid;
+
+}
+
+/*
+ * Utilise the notion of 'unskippable' adapters acting as fixed points,
+ * then calculating length of sublist between these fixed points.
+ * */
+int64_t findSolutionPart2(const vector<int> &vals, bool verbose = false) {
+  vector<int> sorted(vals);
+  sort(sorted.begin(), sorted.end(), [](int a, int b){ return a < b; });
+
+  // Stores indices of unskippable values
+  vector<int> fixedPoints;
+  fixedPoints.push_back(0);
+  int back_diff, front_diff;
+
+  // O(n)
+  for (unsigned int k = 1; k < sorted.size() - 1; k++) {
+    back_diff = sorted.at(k) - sorted.at(k - 1);
+    front_diff = sorted.at(k + 1) - sorted.at(k);
+    // Indicates that this element can't be removed without breaking the chain
+    if (back_diff == 3 || front_diff == 3) fixedPoints.push_back(k);
+    // Means that removing this element would leave a gap of 4, which breaks
+    // the chain
+    else if (back_diff == 2 && front_diff == 2) fixedPoints.push_back(k);
+  }
+
+  // The last value is naturally unskippable
+  fixedPoints.push_back(sorted.size() - 1);
+
+  int64_t numArrangements = 1;
+
+  // O(n)
+  for (unsigned int k = 0; k < fixedPoints.size() - 1; k++) {
+    numArrangements *= calculateCombinations(fixedPoints.at(k+1) - fixedPoints.at(k) + 1); 
+  }
+
+  return numArrangements;
+
+}
+
 void printSolutionsAndTiming(int64_t result, clock_t start, clock_t stop) {
   if (result == -1) cout << "No solution found" << endl;
   else {
@@ -61,7 +132,7 @@ int main(int argc, char ** argv) {
   cout << "## Day 10 of Advent of Code ##" << endl;
 
   vector<int> vals;
-  int result = 0;
+  int64_t result = 0;
   clock_t start, stop;
 
   vals.push_back(0);
@@ -74,14 +145,13 @@ int main(int argc, char ** argv) {
   stop = clock();
 
   printSolutionsAndTiming(result, start, stop);
-  /*
+
   cout << "-- Part 2 --" << endl;
   
   start = clock();
-  result = findSolutionPart2(vals, preamble, 2, false);
+  result = findSolutionPart2(vals, false);
   stop = clock();
 
   printSolutionsAndTiming(result, start, stop);
-  */
 }
 
