9c2c2befee327f3357ea626d1a09aa41d787ef38
Matt Eden <mrmatteden@gmail.com>
2020-12-17 11:11:02 +1300

    Complete Part 2 of Day 12

--- 2020/day-12/ans.cpp
+++ 2020/day-12/ans.cpp
@@ -13,47 +13,41 @@
 using namespace std;
 
 /*
- * Ship has:
- * - direction it's facing (either cardinal or degrees)
+ * Waypoint has:
  * - distance travelled horizontally (east/west)
  * - distance travelled vertically (north/south)
  * */
-class Ship {
-  /* Member fields */
-  private:
-    int facing;
+class Waypoint {
+  protected:
     int horzDist;
     int vertDist;
 
   public:
-    // Constructor
-    Ship() {
-      this->facing = EAST;
+    Waypoint() {
       this->horzDist = 0;
       this->vertDist = 0;
+    } 
+
+    Waypoint(int horzDist, int vertDist) {
+      this->horzDist = horzDist;
+      this->vertDist = vertDist;
     }
 
-    /* Methods */
-    void rotate(char direction, int degrees) {
-      if (direction == 'L') degrees *= -1;
-      this->facing += degrees;
-      if (this->facing < 0) this->facing += 360;
-      if (this->facing >= 360) this->facing -= 360;
-    }
-
-    void moveForward(int distance) {
-      switch (facing) {
-        case NORTH:
-          vertDist += distance;
+    virtual void rotate(char direction, int degrees) {
+      if (direction == 'L') degrees = degrees * -1 + 360; // normalise to clockwise 
+      // Perform rotation based on the notion of quadrants
+      switch (degrees) {
+        case 90:
+          swap(horzDist, vertDist);
+          vertDist *= -1;
           break;
-        case EAST:
-          horzDist += distance;
+        case 180:
+          horzDist *= -1;
+          vertDist *= -1;
           break;
-        case SOUTH:
-          vertDist -= distance;
-          break;
-        case WEST:
-          horzDist -= distance;
+        case 270:
+          swap(horzDist, vertDist);
+          horzDist *= -1;
           break;
       }
     }
@@ -75,11 +69,74 @@ class Ship {
       }
     }
 
+    int getVertDist() { return vertDist; }
+
+    int getHorzDist() { return horzDist; }
+
     int getManhattanDistance() { 
       return abs(horzDist) + abs(vertDist);
     }
+};
+
+/*
+ * Ship has:
+ * - direction it's facing (either cardinal or degrees)
+ * */
+class Ship : public Waypoint {
+  /* Member fields */
+  private:
+    int facing;
+
+  public:
+    // Constructor
+    Ship() {
+      this->facing = EAST;
+    }
+
+    /* Methods */
+    void rotate(char direction, int degrees) {
+      if (direction == 'L') degrees *= -1;
+      this->facing += degrees;
+      if (this->facing < 0) this->facing += 360;
+      if (this->facing >= 360) this->facing -= 360;
+    }
+
+    virtual void moveForward(int distance) {
+      switch (facing) {
+        case NORTH:
+          vertDist += distance;
+          break;
+        case EAST:
+          horzDist += distance;
+          break;
+        case SOUTH:
+          vertDist -= distance;
+          break;
+        case WEST:
+          horzDist -= distance;
+          break;
+      }
+    }
 
 };
+
+class Ferry : public Ship {
+  private:
+    Waypoint* wp;
+
+  public:
+    Ferry(Waypoint* wp) {
+      this->wp = wp;
+    }
+
+    void moveForward(int distance) {
+      vertDist += wp->getVertDist() * distance;
+      horzDist += wp->getHorzDist() * distance;
+    }
+
+    ~Ferry() { delete wp; }
+};
+
 
 /* Function reuse from Day 8 */
 char getDirection(const string &instruction) { return instruction.substr(0, instruction.find_first_of(" "))[0]; }
@@ -106,7 +163,6 @@ int loadDataFromFile(string path, vector<string> &vals) {
   return 0;
 }
 
-
 int findSolutionPart1(const vector<string> &vals) {
   Ship ship;
 
@@ -121,6 +177,24 @@ int findSolutionPart1(const vector<string> &vals) {
   }
 
   return ship.getManhattanDistance();
+
+}
+
+int findSolutionPart2(const vector<string> &vals) {
+  Waypoint *wp = new Waypoint(10, 1);
+  Ferry ferry(wp);
+
+  for (string s : vals) {
+    char direction = getDirection(s);
+    int distance = getDistance(s);
+
+    if (direction == 'F') ferry.moveForward(distance);
+    else if (direction == 'R' || direction == 'L') wp->rotate(direction, distance);
+    else wp->moveDirection(direction, distance);
+
+  }
+
+  return ferry.getManhattanDistance();
 
 }
 
@@ -150,7 +224,6 @@ int main(int argc, char ** argv) {
 
   printSolutionsAndTiming(result, start, stop);
   
-  /*
   cout << "-- Part 2 --" << endl;
   
   start = clock();
@@ -158,6 +231,6 @@ int main(int argc, char ** argv) {
   stop = clock();
 
   printSolutionsAndTiming(result, start, stop);
-  */
+  
 }
 
